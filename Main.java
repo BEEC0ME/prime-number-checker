9ec71a2f7ec8e80b4e8ef7a6e2eb142b772617af
MinJae <ed63533880@gmail.com>
2022-09-15 19:32:55 +0200

    added final check

--- Main.java
+++ Main.java
@@ -5,28 +5,32 @@ public class Main {
         System.out.println("Please input your number");
         Scanner UserNumber = new Scanner(System.in);
         int UserInput = UserNumber.nextInt();
-        //System.out.println(UserInput%2);
+        boolean prime = true;
         if (UserInput%2 == 0){
            
             if (UserInput == 2){
-                System.out.println("This number is a prime number");
+            prime = true;   
             }
             else {
                 System.out.println("This number is not a prime number");
             }
         }
         else {
+        
            for(int i = 2; i < UserInput; i++){
                 //System.out.println(i);
                 int DivisionAnswer = UserInput % i;
                 System.out.println(DivisionAnswer);
                 if (DivisionAnswer == 0){
                     System.out.println("This number is not a prime number");
+                    prime = false; 
+                    break;
                 }
-                else if (DivisionAnswer == 2){
-                    System.out.println("This number is a prime number");
-                }
+ 
            }
+        }
+        if (prime){
+            System.out.println("This number is a prime number");
         }
 
 }
